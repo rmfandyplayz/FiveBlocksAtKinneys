cc890388577cf17090ff42635434b500975c6eaf
rmfandyplayz <andyjia2500@gmail.com>
2025-04-15 10:15:23 -0700

    Update 2

---
+++ Scripts/GameUI/GameUI.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+
+func _on_look_left() -> void:
+	pass # Replace with function body.
+
+func _on_look_right() -> void:
+	pass # Replace with function body.
+
+func _on_look_down() -> void:
+	pass # Replace with function body.
+
+func _on_look_up() -> void:
+	pass # Replace with function body.
