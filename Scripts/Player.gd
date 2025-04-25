43f50881bceb6232ac1237e4a6878d809af52e60
rmfandyplayz <andyjia2500@gmail.com>
2025-04-25 09:56:54 -0700

    Update 6
    more camera stuff + 3d models

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -14,7 +14,12 @@ signal switchCameraMode # switch between looking at computer/looking up
 # 2 = up looking left, 3 = up looking behind, 4 = up looking right
 @export_custom(PROPERTY_HINT_RANGE, "-360,360,or_greater,or_less,degrees") var defaultCamAngle : Vector3
 @export_custom(PROPERTY_HINT_RANGE, "-360,360,or_greater,or_less,degrees") var lookDownCamAngle : Vector3
+@export_custom(PROPERTY_HINT_RANGE, "30,90,or_greater,or_less,degrees") var lookDownCameraFOV : float
+var initialCameraFOV
 
+
+func _ready() -> void:
+	initialCameraFOV = camera.fov
 
 
 func _on_game_ui_turn_camera(direction : int) -> void:
@@ -35,9 +40,12 @@ func _on_game_ui_turn_camera(direction : int) -> void:
 			create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART).tween_property(camera, "rotation_degrees", Vector3(camera.rotation_degrees.x, camera.rotation_degrees.y - 90, camera.rotation_degrees.z), cameraTurnSeconds)
 	
 	elif(direction == 3): # look up
+		create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART).tween_property(camera, "fov", initialCameraFOV, cameraTurnSeconds)
 		create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART).tween_property(camera, "rotation_degrees", defaultCamAngle, cameraTurnSeconds)
 	elif(direction == 4): # look down
 		create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART).tween_property(camera, "rotation_degrees", lookDownCamAngle, cameraTurnSeconds)
+		create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART).tween_property(camera, "fov", lookDownCameraFOV, cameraTurnSeconds)
+	
 	
 	turningCamera.emit(cameraTurnSeconds, direction)
 	await get_tree().create_timer(cameraTurnSeconds).timeout
