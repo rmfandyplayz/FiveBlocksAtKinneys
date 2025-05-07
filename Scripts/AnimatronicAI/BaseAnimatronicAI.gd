3c4d3005333ca324eb8a233d635402bb2a1301ad
rmfandyplayz <andyjia2500@gmail.com>
2025-05-07 09:50:17 -0700

    Update 10
    model import + movement node setup

--- Scripts/AnimatronicAI/BaseAnimatronicAI.gd
+++ Scripts/AnimatronicAI/BaseAnimatronicAI.gd
@@ -1,7 +1,7 @@
 extends Node3D
 class_name BaseAnimatronicAI
 
-@export var aggressionLevel : int # basically works identically to fnaf's AI Level
+@export var aggressionLevel : float # basically works identically to fnaf's AI Level
 @export var actionOpportunity : float # % chance for animatronic to do something each second
 @export var viewDistanceHitbox : Area3D # the area3d that acts as the "hitbox" for the AI
 @export var possibleMoveNodes : Array[BaseAnimatronicAI] ## a list of possible nodes that the animatronic may decide to move to
---
+++ Scripts/AnimatronicAI/MrKinneyAI.gd
@@ -0,0 +1,4 @@
+extends BaseAnimatronicAI
+
+@export var baseAggressionLevel : float
+@export var movesLeftUntilReturn : int ## how many moves left until mr kinney teleports to his table
